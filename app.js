bdaddb52d2255d31b86c9e809e28c837a39fc816
Ronen2345 <ronendo@ymail.com>
2026-01-14 16:00:49 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,2 +1,13 @@
+const express = require('express'); // We need to add express
 const chalk = require('chalk');
-console.log(chalk.blue('Hello World in Blue!'));
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  console.log(chalk.blue('Someone visited the site!'));
+  res.send('<h1 style="color: blue;">Hello World in Blue!</h1>');
+});
+
+app.listen(port, () => {
+  console.log(chalk.green(`Server is live at http://localhost:${port}`));
+});
