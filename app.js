b4ac90b8237d1dfda1ae294f9b388f39b5da44da
Ronen2345 <ronendo@ymail.com>
2026-01-14 16:21:32 +0200

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,14 @@ const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
   console.log(chalk.blue('Someone visited the site!'));
-  res.send('<h1 style="color: blue; font-size: 144px; ">Hello World in Blue!</h1>');
+  res.send(`
+    <div style="text-align: center; margin-top: 50px; font-family: sans-serif;">
+      <h1 style="color: blue; font-size: 48px;">Hello World in Blue!</h1>
+      <button onclick="location.reload()" style="padding: 10px 20px; font-size: 16px; cursor: pointer;">
+        Refresh Page
+      </button>
+    </div>
+  `);
 });
 
 app.listen(port, () => {
